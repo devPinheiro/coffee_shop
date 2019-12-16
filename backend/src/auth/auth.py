50fdfb3f15f1216fb6d08af22b8d6cd249493e22
devPinheiro <samuelpinheiro41@gmail.com>
2019-12-16 12:04:16 +0100

    final submission

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -36,15 +36,24 @@ class AuthError(Exception):
 
 def get_token_auth_header():
     if 'Authorization' not in request.headers:
-        abort(401)
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 401)
 
     auth_header = request.headers['Authorization']
     header_parts = auth_header.split(' ')
 
     if len(header_parts) != 2:
-        abort(401)
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 401)
     elif header_parts[0].lower() != 'bearer':
-        abort(401)
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 401)
 
     return header_parts[1]
 
@@ -74,7 +83,7 @@ def check_permissions(permission, payload):
         raise AuthError({
             'code': 'unauthorized',
             'description': 'Permission not found.'
-        }, 403)
+        }, 401)
     return True
 
 
@@ -181,7 +190,10 @@ def requires_auth(permission=''):
             try:
                 payload = verify_decode_jwt(token)
             except Exception:
-                abort(401)
+                raise AuthError({
+                    'code': 'unauthorized',
+                    'description': 'Permission not found.'
+                }, 401)
 
             check_permissions(permission, payload)
             return f(payload, *args, **kwargs)
